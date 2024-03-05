788cce0daf7857f1f0f053178aa59ac35059cb5b
Abhay Tiwari <abhayskp7948@gmail.com>
2024-03-05 23:25:35 +0530

    created a seprate env file for e2e testing

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import authRoutes from './routes/auth'
 import cookieParser from "cookie-parser"
 
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(cookieParser());
